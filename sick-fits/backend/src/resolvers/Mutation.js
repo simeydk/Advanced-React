d1d7ea0483dc5451a985cc71e0b65f77a4e36398
Simey de Klerk <simeydeklerk@outlook.com>
2018-10-21 21:14:42 +0200

    dummy queries createDog getdog

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,12 @@
-const mutations = {};
+const Mutations = {
+    createDog(parent, args, ctx, info) {
+        global.dogs = global.dogs || []
+        // create a dog
+        const newDog = {name: args.name}
+        global.dogs.push(newDog);
+        console.log(args)
+        return newDog
+    }
+};
 
-module.exports = mutations;
+module.exports = Mutations;
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,8 @@
-const Query = {};
+const Query = {
+    dogs(parent, args, ctx, info) {
+        global.dogs = global.dogs || [{name:'Snickers'},{name:'Snoopy'},{name:'Dobby'}]  
+        return global.dogs
+    }
+};
 
 module.exports = Query;
